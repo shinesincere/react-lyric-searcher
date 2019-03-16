71ee074d232850bc79dca67053bc225da4cdbd7a
shinesincere <shinesincere@gmail.com>
2019-03-16 20:08:17 +0900

    Updates

---
+++ precache-manifest.f9b955ba979114a60160e5c024c4adce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9209e3917db32be3039a613ce26dd618",
+    "url": "/react-lyric-searcher/static/media/spinner.9209e391.gif"
+  },
+  {
+    "revision": "e3894e81b543d549b137",
+    "url": "/react-lyric-searcher/static/js/runtime~main.8a76aeac.js"
+  },
+  {
+    "revision": "5c92b49d69e2b084d8f7",
+    "url": "/react-lyric-searcher/static/js/main.a4cfeda8.chunk.js"
+  },
+  {
+    "revision": "8f1cd2fe2ddc7f69dc71",
+    "url": "/react-lyric-searcher/static/js/2.2ffc6669.chunk.js"
+  },
+  {
+    "revision": "0371a7723f433237d28c00dd8c028cc5",
+    "url": "/react-lyric-searcher/index.html"
+  }
+];
